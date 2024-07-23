66e4e8f4250eeb435033a59f8beb2c4727263d1c
Sagar Melmatti <sagaramelmatti@gmail.com>
2024-07-24 00:48:50 +0530

    Delete one-to-one-mapping directory

--- one-to-one-mapping/src/main/java/com/demo/example/repo/CategoryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.demo.example.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.demo.example.entity.Category;
-
-public interface CategoryRepository extends JpaRepository<Category, Integer> {
-
-}
--- one-to-one-mapping/src/main/java/com/demo/example/repo/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.demo.example.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.demo.example.entity.Product;
-
-public interface ProductRepository extends JpaRepository<Product, Integer> {
-
-}
