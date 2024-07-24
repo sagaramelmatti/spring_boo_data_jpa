4d4d33875396da2226db476995745f4b85ae20f0
sagaramelmatti <sagaramelmatti@gmail.com>
2024-07-24 17:50:02 +0530

    database file committed

--- one-to-one-mapping/database-files/spring_project_db.sql
+++ one-to-one-mapping/database-files/spring_project_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 23, 2024 at 09:06 PM
+-- Generation Time: Jul 24, 2024 at 02:19 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.0.30
 
@@ -38,7 +38,48 @@ CREATE TABLE `category` (
 
 INSERT INTO `category` (`id`, `name`) VALUES
 (1, 'Mens Wear'),
-(2, 'Ladies Wear');
+(2, 'Ladies Wear'),
+(3, 'Kids Wear');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `instructor`
+--
+
+CREATE TABLE `instructor` (
+  `id` int(11) NOT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `first_name` varchar(255) DEFAULT NULL,
+  `last_name` varchar(255) DEFAULT NULL,
+  `instructor_detail_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `instructor`
+--
+
+INSERT INTO `instructor` (`id`, `email`, `first_name`, `last_name`, `instructor_detail_id`) VALUES
+(1, 'sagar@gmail.com', 'sagar', 'melmatti', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `instructor_detail`
+--
+
+CREATE TABLE `instructor_detail` (
+  `id` int(11) NOT NULL,
+  `hobby` varchar(255) DEFAULT NULL,
+  `youtube_channel` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `instructor_detail`
+--
+
+INSERT INTO `instructor_detail` (`id`, `hobby`, `youtube_channel`) VALUES
+(1, 'travelling', 'https://www.youtube.com/watch?v=X48VuDVv0do');
 
 -- --------------------------------------------------------
 
@@ -58,7 +99,9 @@ CREATE TABLE `product_details` (
 --
 
 INSERT INTO `product_details` (`id`, `name`, `price`, `category_id`) VALUES
-(1, 'Formal Shirt', 900, 1);
+(1, 'Formal Shirt', 900, 1),
+(2, 'Casual Shirt', 600, 1),
+(3, 'Kurti Paijama', 900, 2);
 
 --
 -- Indexes for dumped tables
@@ -68,6 +111,19 @@ INSERT INTO `product_details` (`id`, `name`, `price`, `category_id`) VALUES
 -- Indexes for table `category`
 --
 ALTER TABLE `category`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `instructor`
+--
+ALTER TABLE `instructor`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `UKdkf38g56sw6e2flnadv5efx7c` (`instructor_detail_id`);
+
+--
+-- Indexes for table `instructor_detail`
+--
+ALTER TABLE `instructor_detail`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -85,17 +141,35 @@ ALTER TABLE `product_details`
 -- AUTO_INCREMENT for table `category`
 --
 ALTER TABLE `category`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `instructor`
+--
+ALTER TABLE `instructor`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `instructor_detail`
+--
+ALTER TABLE `instructor_detail`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `product_details`
 --
 ALTER TABLE `product_details`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- Constraints for dumped tables
 --
+
+--
+-- Constraints for table `instructor`
+--
+ALTER TABLE `instructor`
+  ADD CONSTRAINT `FK5h8q2s9b2twvpdln31m1q70tw` FOREIGN KEY (`instructor_detail_id`) REFERENCES `instructor_detail` (`id`);
 
 --
 -- Constraints for table `product_details`
