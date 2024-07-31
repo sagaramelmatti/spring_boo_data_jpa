e98d97bfb3840cc08b2abedb2506a733ecf9d090
sagaramelmatti <sagaramelmatti@gmail.com>
2024-07-31 21:49:56 +0530

    database and postman save

--- response-entity-example/database-files/spring_project_db.sql
+++ response-entity-example/database-files/spring_project_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 24, 2024 at 02:19 PM
+-- Generation Time: Jul 31, 2024 at 06:09 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.0.30
 
@@ -24,158 +24,41 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `category`
+-- Table structure for table `employee_details`
 --
 
-CREATE TABLE `category` (
-  `id` int(11) NOT NULL,
-  `name` varchar(20) DEFAULT NULL
+CREATE TABLE `employee_details` (
+  `id` bigint(20) NOT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `role` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `category`
+-- Dumping data for table `employee_details`
 --
 
-INSERT INTO `category` (`id`, `name`) VALUES
-(1, 'Mens Wear'),
-(2, 'Ladies Wear'),
-(3, 'Kids Wear');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `instructor`
---
-
-CREATE TABLE `instructor` (
-  `id` int(11) NOT NULL,
-  `email` varchar(255) DEFAULT NULL,
-  `first_name` varchar(255) DEFAULT NULL,
-  `last_name` varchar(255) DEFAULT NULL,
-  `instructor_detail_id` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `instructor`
---
-
-INSERT INTO `instructor` (`id`, `email`, `first_name`, `last_name`, `instructor_detail_id`) VALUES
-(1, 'sagar@gmail.com', 'sagar', 'melmatti', 1);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `instructor_detail`
---
-
-CREATE TABLE `instructor_detail` (
-  `id` int(11) NOT NULL,
-  `hobby` varchar(255) DEFAULT NULL,
-  `youtube_channel` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `instructor_detail`
---
-
-INSERT INTO `instructor_detail` (`id`, `hobby`, `youtube_channel`) VALUES
-(1, 'travelling', 'https://www.youtube.com/watch?v=X48VuDVv0do');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `product_details`
---
-
-CREATE TABLE `product_details` (
-  `id` int(11) NOT NULL,
-  `name` varchar(50) NOT NULL,
-  `price` double NOT NULL,
-  `category_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `product_details`
---
-
-INSERT INTO `product_details` (`id`, `name`, `price`, `category_id`) VALUES
-(1, 'Formal Shirt', 900, 1),
-(2, 'Casual Shirt', 600, 1),
-(3, 'Kurti Paijama', 900, 2);
+INSERT INTO `employee_details` (`id`, `name`, `role`) VALUES
+(1, 'amit', 'Admin');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `category`
+-- Indexes for table `employee_details`
 --
-ALTER TABLE `category`
+ALTER TABLE `employee_details`
   ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `instructor`
---
-ALTER TABLE `instructor`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `UKdkf38g56sw6e2flnadv5efx7c` (`instructor_detail_id`);
-
---
--- Indexes for table `instructor_detail`
---
-ALTER TABLE `instructor_detail`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `product_details`
---
-ALTER TABLE `product_details`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `category_id` (`category_id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT for table `category`
+-- AUTO_INCREMENT for table `employee_details`
 --
-ALTER TABLE `category`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
---
--- AUTO_INCREMENT for table `instructor`
---
-ALTER TABLE `instructor`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
--- AUTO_INCREMENT for table `instructor_detail`
---
-ALTER TABLE `instructor_detail`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
--- AUTO_INCREMENT for table `product_details`
---
-ALTER TABLE `product_details`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `instructor`
---
-ALTER TABLE `instructor`
-  ADD CONSTRAINT `FK5h8q2s9b2twvpdln31m1q70tw` FOREIGN KEY (`instructor_detail_id`) REFERENCES `instructor_detail` (`id`);
-
---
--- Constraints for table `product_details`
---
-ALTER TABLE `product_details`
-  ADD CONSTRAINT `product_details_ibfk_1` FOREIGN KEY (`category_id`) REFERENCES `category` (`id`);
+ALTER TABLE `employee_details`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
